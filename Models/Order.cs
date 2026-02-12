0d704bed133d9dfdac04c570881eaf44ed9b4a1f
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2026-02-13 02:39:00 +0530

    Create Order.cs

---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+namespace EcommerceAPI.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public decimal TotalAmount { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+}
