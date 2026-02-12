decf9370f21e30d00e6e5fa1ece670c275bbdf14
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2026-02-13 02:21:59 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,9 @@
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+app.UseCors("AllowAll");
